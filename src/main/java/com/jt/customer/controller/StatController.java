27ac9c911750a2fa7f8a82b2217f503d02890c51
JoyLinger <LinJi29@outlook.com>
2022-11-25 16:41:08 +0800

    修复echarts走势图日期不连续问题

--- src/main/java/com/jt/customer/controller/StatController.java
+++ src/main/java/com/jt/customer/controller/StatController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -54,22 +55,24 @@ public class StatController {
   public JSONObject incomeChart(Integer durPeriod) {
     LOG.info("durPeriod={}", durPeriod);
     List<IncomeInterface> incomes = new ArrayList<>();
-    String dateFormat;
+    String dateFormat4sql = "%Y-%m-%d", dateFormat4java = "yyyy-MM-dd";
     switch (durPeriod) {
       case 1:
-        dateFormat = "%Y-%m-%d";
+        dateFormat4sql = "%Y-%m-%d";
+        dateFormat4java = "yyyy-MM-dd";
         break;
       case 2:
-        dateFormat = "%Y-%m";
+        dateFormat4sql = "%Y-%m";
+        dateFormat4java = "yyyy-MM";
         break;
       case 3:
-        dateFormat = "%Y";
+        dateFormat4sql = "%Y";
+        dateFormat4java = "yyyy";
         break;
       default:
         LOG.warn("不合法的统计周期:{},默认按天统计", durPeriod);
-        dateFormat = "%Y-%m-%d";
     }
-    incomes = statService.statIncomeByDateFormat(dateFormat);
+    incomes = statService.statIncomeByDateFormat(dateFormat4sql);
     String title = "收入统计";
     List<String> legends = new ArrayList<>();
     legends.add("收入");
@@ -86,6 +89,8 @@ public class StatController {
       int income_y1 = income.getIncome();
       int population_y2 = income.getPopulation();
       if (i == 0) {
+        // 首次迭代: 直接放入xDataList
+        xDataList.add(date_x);
         // 首次迭代: 直接放入seriesMap
         List<Integer> income_y1_list = new ArrayList<>();
         income_y1_list.add(income_y1);
@@ -94,15 +99,25 @@ public class StatController {
         population_y2_list.add(population_y2);
         seriesMap.put("顾客人数", population_y2_list);
       } else {
-        // 多次迭代: 更新seriesMap中老的系列值
+        // 多次迭代: 补全x轴间断日期及其对应y轴数据
+        List<String> gapDates = getGapDates(xDataList.get(xDataList.size() - 1), date_x, dateFormat4java);
+        // 补全x轴间断日期
+        xDataList.addAll(gapDates);
+        // 更新seriesMap中老的系列值
         List<Integer> income_y1_list = seriesMap.get("收入");
-        income_y1_list.add(income_y1);
-        seriesMap.put("收入", income_y1_list);
         List<Integer> population_y2_list = seriesMap.get("顾客人数");
+        // 补全对应y轴数据
+        for(int y=0;y<gapDates.size();y++){
+          income_y1_list.add(0);
+          population_y2_list.add(0);
+        }
+        // 最后加上本次迭代数据
+        xDataList.add(date_x);
+        income_y1_list.add(income_y1);
         population_y2_list.add(population_y2);
+        seriesMap.put("收入", income_y1_list);
         seriesMap.put("顾客人数", population_y2_list);
       }
-      if (!xDataList.contains(date_x)) xDataList.add(date_x);
     }
     for (Map.Entry<String, List<Integer>> entry : seriesMap.entrySet()) {
       EChartsSeries series1 = new EChartsSeries(entry.getKey(), entry.getValue());
@@ -119,6 +134,60 @@ public class StatController {
     JSONObject jsonObj = JSONObject.parseObject(eChartsOption.toString());
     LOG.info("jsonObj={}", jsonObj);
     return jsonObj;
+  }
+
+  /**
+   * 获取两个日期之间的跨度
+   * @param beginDate 开始日期
+   * @param endDate 结束日期
+   * @param dateFormat 日期格式, 目前支持日(%Y-%m-%d), 月(%Y-%m), 年(%Y)
+   * @return 跨度的日期列表, 没有跨度则返回空列表
+   */
+  private List<String> getGapDates(String beginDate, String endDate, String dateFormat) {
+    List<String> gapDates = new ArrayList<>();
+    Calendar beginCalendar = Calendar.getInstance();
+    Calendar endCalendar = Calendar.getInstance();
+    SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+    try {
+      Date bd = sdf.parse(beginDate);
+      Date ed = sdf.parse(endDate);
+      beginCalendar.setTime(bd);
+      endCalendar.setTime(ed);
+    } catch (ParseException e) {
+      LOG.error(e.toString(), e);
+      e.printStackTrace();
+    }
+    // 日期跨度
+    long minus = 1;
+    switch (dateFormat) {
+      case "yyyy-MM-dd":
+        long ms = endCalendar.getTimeInMillis() - beginCalendar.getTimeInMillis();
+        // 相差天数
+        long days = ms / 1000 / 3600 / 24;
+        minus = days;
+        break;
+      case "yyyy-MM":
+        int minusMonth = endCalendar.get(Calendar.MONTH) - beginCalendar.get(Calendar.MONTH);
+        int minusYear = endCalendar.get(Calendar.YEAR) - beginCalendar.get(Calendar.YEAR);
+        // 相差几个月
+        int months = minusMonth + minusYear * 12;
+        minus = months;
+        break;
+      case "yyyy":
+        // 相差几年
+        int years = endCalendar.get(Calendar.YEAR) - beginCalendar.get(Calendar.YEAR);
+        minus = years;
+        break;
+      default:
+        LOG.error("不合法的dateFormat:{}, 不做任何处理!", dateFormat);
+    }
+    if(minus != 1){
+      for (;minus > 1;minus--){
+        beginCalendar.add(Calendar.DATE, 1);
+        gapDates.add(sdf.format(beginCalendar.getTime()));
+      }
+    }
+    return gapDates;
   }
 
   @RequestMapping("/filter/stats/customerHome")
@@ -139,15 +208,19 @@ public class StatController {
   public JSONObject customerChart(Integer durPeriod) {
     LOG.info("durPeriod={}", durPeriod);
     List<CustomerInterface> customers = new ArrayList<>();
+    String dateFormat4java = "yyyy-MM-dd";
     switch (durPeriod) {
       case 1:
         customers = statService.statCustomerByDay();
+        dateFormat4java = "yyyy-MM-dd";
         break;
       case 2:
         customers = statService.statCustomerByMonth();
+        dateFormat4java = "yyyy-MM";
         break;
       case 3:
         customers = statService.statCustomerByYear();
+        dateFormat4java = "yyyy";
         break;
       default:
         LOG.info("不合法的统计周期:{}", durPeriod);
@@ -159,24 +232,58 @@ public class StatController {
     yAxisList.add(new ECharts_yAxis("顾客量","left","人次"));
     List<EChartsSeries> seriesList = new ArrayList<>();
     Map<String, List<Integer>> seriesMap = new HashMap<>();
-    for (CustomerInterface customer : customers) {
+    for (int i=0;i<customers.size();i++) {
+      CustomerInterface customer = customers.get(i);
       String legend = StaticContrast.parseRole(customer.getRole());
       String date_x = customer.getDate();
       int value_y = customer.getTotal();
-      if (!legends.contains(legend)) {
+      if (i == 0) {
+        // 首次迭代: 直接放入legends
         legends.add(legend);
-        // 不同系列: 直接放入seriesList
+        // 首次迭代: 直接放入xDataList
+        xDataList.add(date_x);
+        // 首次迭代: 直接放入seriesMap
         List<Integer> values = new ArrayList<>();
         values.add(value_y);
         seriesMap.put(legend, values);
       } else {
-        // 同一个系列: 更新seriesList中老的系列值
-        List<Integer> oldValues = seriesMap.get(legend);
-        oldValues.add(value_y);
-        seriesMap.put(legend, oldValues);
+        if (!legends.contains(legend)) {
+          // 去重放入xDataList
+          if(!xDataList.contains(date_x)) xDataList.add(date_x);
+          // 多次迭代同一日期(x)不同角色(系列)
+          legends.add(legend);
+          // 不同角色(系列): 直接放入seriesList
+          List<Integer> values = new ArrayList<>();
+          values.add(value_y);
+          seriesMap.put(legend, values);
+        } else {
+          if (legend.equals(legends.get(0))) {
+            // 多次迭代不同日期(x): 补全x轴间断日期及其对应y轴数据
+            List<String> gapDates = getGapDates(xDataList.get(xDataList.size() - 1), date_x, dateFormat4java);
+            // 补全x轴间断日期
+            xDataList.addAll(gapDates);
+            for(String gapDate : gapDates) {
+              for (String legendTemp : legends) {
+                // 更新seriesMap中老的系列值: 所有角色(系列)都要更新
+                List<Integer> oldValues = seriesMap.get(legendTemp);
+                // 补全对应y轴数据
+                oldValues.add(0);
+                seriesMap.put(legendTemp, oldValues);
+              }
+            }
+          }
+          // 最后加上本次迭代数据
+          // 去重放入xDataList
+          if(!xDataList.contains(date_x)) xDataList.add(date_x);
+          // 更新seriesMap中老的系列值: 所有角色(系列)都要更新
+          List<Integer> oldValues = seriesMap.get(legend);
+          // 同一个系列: 更新seriesList中老的系列值
+          oldValues.add(value_y);
+          seriesMap.put(legend, oldValues);
+        }
       }
-      if (!xDataList.contains(date_x)) xDataList.add(date_x);
     }
+    Collections.sort(xDataList);
     for (Map.Entry<String, List<Integer>> entry : seriesMap.entrySet()) {
       EChartsSeries series1 = new EChartsSeries(entry.getKey(), entry.getValue());
       seriesList.add(series1);
